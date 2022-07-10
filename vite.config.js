8812410fca221b141f3731b3c1b1b153f82d74a6
eguarnizo77 <eduguarny20@gmail.com>
2022-07-09 21:58:12 -0500

    creation pwa vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*{js,css,html,ico,png,svg}']
+      },  
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/iconx512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }    
+    }),    
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/img/iconx512.png',
+          dest: 'assets'
+        },
+        {
+          src: 'src/assets/img/iconx128.png',
+          dest: 'assets'
+        },
+        {
+          src:  'manifest.json',
+          dest: ''
+        }
+      ]
+    })
+  ]
 })
